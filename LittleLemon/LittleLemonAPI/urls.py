a7817d9e2d195e40ac2d4b7f0a760721957caed3
NicolasMironRamoss <nicolas.miron.ramos@accenture.com>
2024-12-26 15:10:00 +0000

    Module 2: Serializers

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 url_patterns = [
     path('menu-items', views.MenuItemsView.as_view()),
-    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    # path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/<int:id>', views.single_item),
     ]
