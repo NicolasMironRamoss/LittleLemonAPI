7831fb2dabcc746a4ca46955787e718e681e4619
NicolasMironRamoss <nicolas.miron.ramos@accenture.com>
2024-12-26 11:45:27 +0000

    .

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-url_patterns = [path('menu-items', views.MenuItemsView.as_view()),]
+url_patterns = [
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    ]
