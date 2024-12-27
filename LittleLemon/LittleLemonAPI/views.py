61e87bbfe6ec90b288e9b55c9155b6f8c6833ec7
NicolasMironRamoss <nicolas.miron.ramos@accenture.com>
2024-12-27 10:01:01 +0000

    Module 2: Relationship Serializers

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -7,19 +7,11 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
-# class MenuItemsView(generics.ListCreateAPIView):
-#     queryset = MenuItem.objects.all()
-#     serializer_class = MenuItemSerializer
-
-# class SingleMenuItemView(generics.RetrieveUpdateAPIReview, generics.DestroyAPIView):
-#     queryset = MenuItem.objects.all()
-#     serializer_class = MenuItemSerializer
 
 # SERIALIZERS:
-# Create your views here.
 @api_view()
 def menu_items(request):
-    items = MenuItem.objects.all()
+    items = MenuItem.objects.select_related('category').all()
     serialized_item = MenuItemSerializer(items, many=True)
     return Response(serialized_item.data)
 
@@ -28,3 +20,13 @@ def single_item(request, id):
     item = get_object_or_404(MenuItem, pk=id)
     serialized_item = MenuItemSerializer(item)
     return Response(serialized_item.data)
+
+
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
+# class SingleMenuItemView(generics.RetrieveUpdateAPIReview, generics.DestroyAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
